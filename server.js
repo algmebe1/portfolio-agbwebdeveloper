42d093b98e51648486db6273b289d8faee7674b1
algmebe1 <algmebe1@gmail.com>
2021-03-16 21:05:41 +0100

    server.js updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const cors = require('cors')
 const nodemailer = require('nodemailer')
-// const path = require('path')
+const path = require('path')
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 app.use('/', router)
-/* app.use(express.static(path.join(__dirname, 'client', 'build'))) */
+app.use(express.static(path.join(__dirname, 'client', 'build')))
 /* app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
 }) */
