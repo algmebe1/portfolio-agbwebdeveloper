e9fa5ced03948596033a223c62b068ff305fc963
algmebe1 <algmebe1@gmail.com>
2021-03-16 19:41:07 +0100

    server.js updated

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const nodemailer = require('nodemailer')
 const path = require('path')
 
 const app = express()
-const port = process.env.PORT || 5000
+// const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'client', 'build')))
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
 })
-app.listen(port, () => console.log('Server Running'))
+app.listen(5000, () => console.log('Server Running'))
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
