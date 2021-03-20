6259d7e965fb6d3af21d1085b0bc054bd274f024
algmebe1 <algmebe1@gmail.com>
2021-03-20 19:43:51 +0100

    menu refresh depending on screen width and logo replaced

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -1,19 +1,29 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './Navigation.css';
 import { Link, animateScroll as scroll } from 'react-scroll';
 
 function Navigation() {
+  const [width, setWidth] = useState(window.innerWidth)
   const scrollToTop = () => {
     scroll.scrollToTop();
   };
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setWidth(window.innerWidth)
+    })  
+  }, [width])
+
 
   return (
     <nav className="header__nav-buttons">
       <div>
-        <img src="https://raw.githubusercontent.com/spothq/cryptocurrency-icons/9ab8d6934b83a4aa8ae5e8711609a70ca0ab1b2b/svg/color/kcs.svg" alt="logo" onClick={scrollToTop} />
+        <img src="https://drive.google.com/uc?id=1hORgp_-iee8zbiLiZ6U8TH2QyjBVRQLL" alt="logo" className="header-logo" onClick={scrollToTop} />
       </div>
+    {width > 768 ? 
+      (
+      <>
       <ul>
         <li className="about-link">
           <Link
@@ -60,6 +70,62 @@ function Navigation() {
           </Link>
         </li>
       </ul>
+      </>
+      ) : (
+      <div className="dropdown">
+        <button type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fa fa-bars"></i>
+        </button>
+        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <button className="dropdown-item">
+            <Link
+              activeClass="active"
+              to="about"
+              smooth
+              offset={-100}
+              duration={500}
+            >
+              <span className="dropdownText">ABOUT</span>
+            </Link>
+          </button>
+          <button className="dropdown-item">
+            <Link
+              activeClass="active"
+              to="skills"
+              smooth
+              offset={-100}
+              duration={500}
+            >
+              <span className="dropdownText">SKILLS</span>
+            </Link>
+          </button>
+          <button className="dropdown-item">
+            <Link
+              activeClass="active"
+              to="projects"
+              smooth
+              offset={-100}
+              duration={500}
+            >
+              <span className="dropdownText">PROJECTS</span>
+            </Link>
+          </button>
+          <button className="dropdown-item">
+            <Link
+              activeClass="active"
+              to="contact"
+              smooth
+              offset={-100}
+              duration={500}
+            >
+              <span className="dropdownText">CONTACT</span>
+            </Link>
+          </button>
+        </div>
+      </div>
+
+      
+      )}
     </nav>
   );
 }
